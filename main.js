e82717aae6fba341060522d0773bdff88e361d34
assortest <2861854011@qq.com>
2026-08-23 23:50:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ try {
     2642:"ffxivboot.exe,ffxivboot64.exe,ffxivlauncher.exe,FINAL FANTASY XIV.exe,ffxiv_dx11.exe,ffxiv.exe",//ff14
     8668:"Arknights.exe,MuMuNxDevice.exe",//明日方舟国服
     2223:"MuMuNxDevice.exe,NemuService.exe,NemuPlayer.exe",//明日方舟国际服
+    11006:"How to Fish.exe",//渔力全开
   };
   const ExcludedGameIDs = [109, 437, 274, 1921, 1342, 860, 2529, 4371]; //steam epic 育碧uplay eaapp  rockstar GOG 远程同乐 碧蓝幻想
   const UI_STATES = {
